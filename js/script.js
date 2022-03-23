727cb4b93326d16217479a0f6231f8b411b2c51b
Brayden MacMillan <99274619+BraydenMacMillan7@users.noreply.github.com>
2022-03-23 20:23:29 +0000

    finished everything

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Brayden MacMillan
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function calcAreaPer () {
+  // this function does basic math
+
+  document.getElementById('calculate-area').innerHTML = 'The area is ' + (200*75) + 'cm²'
+}
+
+function myButtonClicked () {
+  // this function does basic math
+  
+document.getElementById('calculate-per').innerHTML = 'The perimeter is ' + (275 * 2) + 'cm'
+}
+
+
